fab30923e294efe093342c5f9e561fe75343ee9e
Yuri Andrade <yfm.andrade@gmail.com>
2025-10-08 04:35:44 -0300

    chore(config): add project configuration files 🔧
    - Add Angular configuration files
    - Set up testing environment with Karma
    - Add package dependencies
    - Configure environment variables

---
+++ pokedex-challenge/karma.conf.js
@@ -0,0 +1,35 @@
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-safari-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      jasmine: {
+        // you can add configuration options for Jasmine here
+        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+        // for example, you can disable the random execution with `random: false`
+        // or set a specific seed with `seed: 4321`
+      },
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' }
+      ]
+    },
+    reporters: ['progress', 'kjhtml'],
+    browsers: ['Safari'],
+    failOnEmptyTestSuite: false,
+    logLevel: config.LOG_DEBUG,
+    restartOnFileChange: true
+  });
+};
