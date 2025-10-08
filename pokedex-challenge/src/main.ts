0232acc68d192781cf6dd29a34722fd4e0949170
Yuri Andrade <yuriandrade@Yuris-Mac-mini.local>
2025-10-08 04:31:48 -0300

    chore: initial project setup 🎉
    - Add TypeScript configuration files
    - Configure base HTML template
    - Add global styles and typography
    - Setup main Angular bootstrapping file

---
+++ pokedex-challenge/src/main.ts
@@ -0,0 +1,9 @@
+// src/main.ts
+
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { App } from './app/app';
+
+
+bootstrapApplication(App, appConfig)
+  .catch((err) => console.error(err));
